90a910d7f4ceb1930736a9c956bbfad58f2b1e21
Selena Deckelmann <selenamarie@gmail.com>
2015-01-16 16:52:41 -0800

    Setting up initial model

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import *
 
 @app.route('/')
 def index():
