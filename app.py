787dbb1a73d6bcfc9015676b13f132f53abbcd3d
Selena Deckelmann <selenamarie@gmail.com>
2015-02-02 09:38:52 -0800

    add run_query()

--- app.py
+++ app.py
@@ -40,17 +40,35 @@ def new_query_source():
         return jsonify({'status': 'ok', 'id': id}), 201
 
 @app.route('/database/queries/<int:query_id>', methods=['GET'])
-def get_query_source(query_id):
+def get_query(query_id):
     if request.method == 'GET':
         try:
             user_query = QuerySource.query.filter_by(id=query_id).first()
             return jsonify({'status': 'ok', 'query': user_query.query_text})
+        # TODO more interesting e handling
         except Exception as e:
             return jsonify({
                 'status': 'not ok',
-                'error': 'Could not execute query',
+                'error': 'Could not find query',
                 'exception': str(e)
             }), 401
+
+@app.route('/database/queries/<int:query_id>/run', methods=['GET'])
+def run_query(query_id):
+    if request.method == 'GET':
+        try:
+            user_query = QuerySource.query.filter_by(id=query_id).first()
+            results = db.session.execute(user_query.query_text)
+            # http://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
+            return jsonify({'results': results}), 201
+        # TODO more interesting e handling
+        except Exception as e:
+            return jsonify({
+                'status': 'not ok',
+                'error': 'Could not find specified query query',
+                'exception': str(e)
+            }), 401
+
 
 
 if __name__ == '__main__':
