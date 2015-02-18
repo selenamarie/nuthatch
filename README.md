0a6abc5815fadd5023c2a7507bb37f21ded98996
Selena Deckelmann <selenamarie@gmail.com>
2015-02-18 11:28:02 -0800

    Adding some celery

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nuthatch
 System for querying and returning data sets
+
+# Installation
+
+Set up a virtualenv and install packages. 
+
+Current environment requires Redis. Installation instructions that were used are here: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-redis
+
+Heroku Redis/celery instructions: https://devcenter.heroku.com/articles/celery-heroku
--- app.py
+++ app.py
@@ -1,20 +1,56 @@
 #!/usr/bin/env python
 
 import boto
-from flask import Flask, request, jsonify
+from celery import Celery
+from celery.signals import task_prerun
+from flask import Flask, request, jsonify, g
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 import requests
 
 app = Flask(__name__)
+
+# Set up Postgres database connection
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
 from models import *
 
+# Set up Celery
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379',
+    CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml'],
+)
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    celery.app = app
+    return celery
+
+celery = make_celery(app)
+#print celery.app
+
+@task_prerun.connect
+def celery_prerun(*args, **kwargs):
+    #print g
+    with celery.app.app_context():
+    #   # use g.db
+       print g
+
+
 @app.route('/')
 def index():
     return "Hello, World!"
+
 
 @app.route('/database/queries/new', methods=['POST'])
 def new_query_source():
@@ -39,6 +75,7 @@ def new_query_source():
 
         return jsonify({'status': 'ok', 'id': id}), 201
 
+
 @app.route('/database/queries/<int:query_id>', methods=['GET'])
 def get_query(query_id):
     if request.method == 'GET':
@@ -53,22 +90,44 @@ def get_query(query_id):
                 'exception': str(e)
             }), 401
 
+
 @app.route('/database/queries/<int:query_id>/run', methods=['GET'])
-def run_query(query_id):
+def queue_query(query_id):
     if request.method == 'GET':
         try:
-            user_query = QuerySource.query.filter_by(id=query_id).first()
-            results = db.session.execute(user_query.query_text)
-            # http://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
-            return jsonify({'results': results}), 201
-        # TODO more interesting e handling
+            run_query.delay(query_id)
+            return jsonify({'status': 'ok', 'enqueued': 'yes', 'id': query_id}), 201
         except Exception as e:
             return jsonify({
                 'status': 'not ok',
-                'error': 'Could not find specified query query',
+                'error': 'Unable to add query to system',
                 'exception': str(e)
             }), 401
 
+
+@app.route('/database/queries/<int:query_id>/latest_result', methods=['GET'])
+def query_result(query_id):
+        myresult = Results.query.filter_by(id=query_id).ordery_by(id).last()
+        return jsonify({'results': myresult.result_all}), 201
+
+
+@celery.task
+def run_query(query_id):
+    try:
+        user_query = QuerySource.query.filter_by(id=query_id).first()
+        results = db.session.execute(user_query.query_text)
+        # http://stackoverflow.com/questions/5022066/how-to-serialize-sqlalchemy-result-to-json
+        # need to store the result, and then make it available at the DB url - so we need a mapping of s3 to query id s
+        newresult = Result(query_id=query_id, result_all=jsonify(results))
+        db.session.add(newresult)
+        db.session.commit()
+    except Exception as e:
+        print e
+        #return {
+            #'status': 'not ok',
+            #'error': 'Could not run specified query',
+            #'exception': str(e)
+        #}
 
 
 if __name__ == '__main__':
